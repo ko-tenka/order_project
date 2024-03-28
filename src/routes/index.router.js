2a2f177533b35fb1a2c923435a1d449cdad1a6db
Dmitry Tarasov <148873767+Dmitriy595@users.noreply.github.com>
2024-03-28 14:15:11 +0300

    chat

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -6,14 +6,15 @@ const AddBook = require('../views/AddBook');
 const { Book, Izbrannoe } = require('../../db/models');
 const ProBook = require('../views/ProBook');
 const Favorites = require('../views/Favorites');
-const { Rate} = require('../../db/models');
+const { Rate, Comment } = require('../../db/models');
 
-indexRouter.get(`/probook/:id`, async (req, res) => {
+indexRouter.get('/probook/:id', async (req, res) => {
   try {
     const { login } = req.session;
-    const book = await Book.findOne({ where: { id: req.params.id } })
-    console.log("не наход? ------------",book)
-    renderTemplate(ProBook, { login, book }, res);
+    const book = await Book.findOne({ where: { id: req.params.id } });
+    const comments = await Comment.findAll({ where: { book_id: req.params.id } });
+    // console.log('не наход? ------------', book);
+    renderTemplate(ProBook, { login, book, comments }, res);
   } catch (error) {
     console.log('ERROR', error);
   }
@@ -22,48 +23,31 @@ indexRouter.get(`/probook/:id`, async (req, res) => {
 indexRouter.get('/', async (req, res) => {
   try {
     const { login } = req.session;
-    
+
     const allRates = await Rate.findAll({ raw: true });
     const books = await Book.findAll({ raw: true });
 
     const ratings = {};
 
     // Вычисляем общий рейтинг для каждой книги
-    books.forEach(book => {
+    books.forEach((book) => {
       const bookId = book.id;
-      const bookRatings = allRates.filter(rate => rate.book_id === bookId);
+      const bookRatings = allRates.filter((rate) => rate.book_id === bookId);
       const totalStars = bookRatings.reduce((total, rate) => total + rate.stars, 0);
-      if(totalStars === 0){
-        ratings[bookId]=0
-      }else{
-      ratings[bookId] = (totalStars / bookRatings.length).toFixed(1); 
-    }
+      if (totalStars === 0) {
+        ratings[bookId] = 0;
+      } else {
+        ratings[bookId] = (totalStars / bookRatings.length).toFixed(1);
+      }
     });
-    console.log(ratings)
+    console.log(ratings);
     // Передаем результаты рендеринга шаблону
     renderTemplate(Home, { login, books, ratings }, res);
-    
-
   } catch (error) {
     console.log('ERROR', error);
     res.status(500).send('Internal Server Error');
-  }});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  }
+});
 
 indexRouter.get('/404', (req, res) => {
   const { login } = req.session;
--- src/routes/rate.router.js
+++ src/routes/rate.router.js
@@ -1,70 +1,29 @@
 const express = require('express');
 const session = require('express-session');
+
 const router = express.Router();
-const { Rate, Comment } = require('../../db/models');
+const { Rate } = require('../../db/models');
 
 router.post('/', async (req, res) => {
   try {
-// ===================================
+    const { rate, book_id } = req.body;
+    const user_id = req.session.userId.id;
 
-// router.get('/', async (req, res) => {
-//   try {
-//     const userId = req.params.user.id;
-//     const bookId = req.params.book.id;
-//     const comments = await Comment.findAll({ where: { userId: user_id, bookId: book_id } });
-//     if (comments.length > 0) {
-//       const latestComment = comments[comments.length - 1];
-//       res.json({ latestComment });
-//     } else {
-//       res.json({ message: 'Коментов нет' });
-//     }
-//     res.json(comments);
-//   } catch (err) {
-//     console.log('Error on  ====>>>>', err);
-//   }
-// });
-
-// router.post('/', async (req, res) => {
-//   try {
-//     // const {
-//     //   comment,
-//     // } = req.body;
-//     // const user_id = req.session.userId;
-//     // const book_id = req.session.bookId;
-//     const newComment = await Comment.create({
-//       user_id: req.body.user_id,
-//       book_id: req.body.book_id,
-//       comment: req.body.comment,
-//     });
-//     res.json(newComment);
-//   } catch (err) {
-//     console.log('Error on  ====>>>>', err);
-//     res.status(500).send('ничего не добавилось');
-//   }
-// });
-
-
-      const { rate, book_id } = req.body;
-      const user_id = req.session.userId.id;
-
-      const existingRate = await Rate.findOne({ where: { book_id, user_id } });
-      if (existingRate) {
-          res.status(400).json({ err: 'Запись уже существует' });
-      }else{
-
+    const existingRate = await Rate.findOne({ where: { book_id, user_id } });
+    if (existingRate) {
+      res.status(400).json({ err: 'Запись уже существует' });
+    } else {
       const newTask = await Rate.create({
-          stars: rate,
-          user_id,
-          book_id
+        stars: rate,
+        user_id,
+        book_id,
       });
       res.status(200).json({ starDone: 'Ставим оценку' });
     }
-    
   } catch (err) {
-      console.log('Error on taskRouter.post() ====>>>>', err);
-      res.status(500).send('Error creating task');
+    console.log('Error on taskRouter.post() ====>>>>', err);
+    res.status(500).send('Error creating task');
   }
 });
 
 module.exports = router;
-
